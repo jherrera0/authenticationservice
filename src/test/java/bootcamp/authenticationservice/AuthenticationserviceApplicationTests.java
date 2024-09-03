7f9c2113bb3c301eb13c0246f8cb1aaf8a34a7fd
Jose Herrera <jherreraa2@unicartagena.edu.co>
2024-09-02 22:54:46 -0500

    Initial commit

---
+++ src/test/java/bootcamp/authenticationservice/AuthenticationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
